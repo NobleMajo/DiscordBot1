2288d43844a593a627b49f30280dc0692eda3edd
konevskib@hotmail.com <konevskib@hotmail.com>
2021-08-11 20:53:41 +0200

    p

---
+++ DiscordBot/Modules/BasicCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Net;
+using Discord.Commands;
+
+namespace DiscordBot.Modules
+{
+    public class BasicCommands : ModuleBase<SocketCommandContext>
+    {
+        [Command("hi")]
+        public async Task hi()
+        {
+            await ReplyAsync("Hello there!");
+        }
+
+    }
+}
