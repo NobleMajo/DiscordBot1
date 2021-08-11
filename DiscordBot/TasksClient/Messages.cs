5189c608abe29ec7d205def806d4bd543f3df733
era6-6-6 <sindler.martin1@email.cz>
2021-08-11 19:15:02 +0200

    msg counter

---
+++ DiscordBot/TasksClient/Messages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Discord.Commands;
+using Discord.Net;
+using Discord;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace DiscordBot.TasksClient
+{
+    public class Messages : ModuleBase<SocketCommandContext>
+    {
+        public static int MsgCount { get; set; } = 0;
+        public async Task MsgCout(SocketMessage msg)
+        {
+            if(!msg.Author.IsBot)
+            MsgCount++;
+        }
+
+    }
+}
