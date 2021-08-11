2789556759aa8dce70a4999256bc83bedb75b84d
era6-6-6 <sindler.martin1@email.cz>
2021-08-11 19:58:25 +0200

    Update Messages.cs

--- DiscordBot/TasksClient/Messages.cs
+++ DiscordBot/TasksClient/Messages.cs
@@ -4,16 +4,20 @@ using Discord.Net;
 using Discord;
 using System.Threading.Tasks;
 using Discord.WebSocket;
+using DiscordBot;
 
 namespace DiscordBot.TasksClient
 {
     public class Messages : ModuleBase<SocketCommandContext>
     {
+        Form1 btns;
         public static int MsgCount { get; set; } = 0;
         public Task MsgCouter(SocketMessage msg)
         {
+            btns = new Form1();
             if(!msg.Author.IsBot)
             MsgCount++;
+            btns.serverMessagesButton.Text = MsgCount.ToString();
             return Task.CompletedTask;
         }
 
