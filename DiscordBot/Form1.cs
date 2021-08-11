6cbc97c6919ffaf8ddbbdc25e30294396540c6b9
Konev-PC <Konev-PC@DESKTOP-S9IOSM5>
2021-08-11 12:46:35 +0200

    Add project files.

---
+++ DiscordBot/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MetroFramework;
+using MetroFramework.Components;
+using MetroFramework.Forms;
+
+namespace DiscordBot
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
