54031b2b915c58a85ee726bfaffc93c7c7a24a30
konevskib@hotmail.com <konevskib@hotmail.com>
2021-08-12 20:15:59 +0200

    gdf

--- DiscordBot/Modules/BasicCommands.cs
+++ DiscordBot/Modules/BasicCommands.cs
@@ -7,6 +7,7 @@ using Discord.Net;
 using Discord.Commands;
 using Discord;
 using System.Windows.Forms;
+using Discord.WebSocket;
 
 namespace DiscordBot.Modules
 {
@@ -65,6 +66,28 @@ namespace DiscordBot.Modules
                     }
             }
 
+        }
+        [Command("kick")]
+        public async Task Kick(SocketGuildUser userAccount, string reason)
+        {
+            var user = Context.User as SocketGuildUser;
+            var role = (user as IGuildUser).Guild.Roles.FirstOrDefault(x => x.Name == "Admin");
+            if (!userAccount.Roles.Contains(role))
+            {
+                if (user.GuildPermissions.KickMembers)
+                {
+                    await userAccount.KickAsync(reason);
+                    await Context.Channel.SendMessageAsync($"The user `{userAccount}` has been kicked, for {reason}");
+                }
+                else
+                {
+                    await Context.Channel.SendMessageAsync("No permissions for kicking a user.");
+                }
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("This User can't be kicked, because the user has a admin role.");
+            }
         }
 
 
