07f24f4719d14ff2f7a3376c4edadf896c59f785
martin šindler <sindler.martin1@email.cz>
2021-08-14 01:26:57 +0200

    code Clean

--- DiscordBot/SafeThreading/SafeThreadingForm.cs
+++ DiscordBot/SafeThreading/SafeThreadingForm.cs
@@ -12,68 +12,6 @@ namespace DiscordBot.SafeThreading
     {
         //all safe thread calls should be there 
         static Form1 myForm = Application.OpenForms.OfType<Form1>().FirstOrDefault();
-        public void ServerNameText(string text)
-        {
-            if (myForm.ServerButton.InvokeRequired)
-            {
-                Action safeWrite = delegate { ServerNameText(text); };
-                myForm.ServerButton.Invoke(safeWrite);
-            }
-            else
-                myForm.ServerButton.Text = text;
-        }
-
-        public void ServerOwner(string text)
-        {
-            if (myForm.OwnerButton.InvokeRequired)
-            {
-                Action safeWrite = delegate { ServerOwner(text); };
-                myForm.OwnerButton.Invoke(safeWrite);
-            }
-            else
-                myForm.OwnerButton.Text = text;
-        }
-
-        public void TotalMembers(string text)
-        {
-            if (myForm.MembersButton.InvokeRequired)
-            {
-                Action safeWrite = delegate { TotalMembers(text); };
-                myForm.MembersButton.Invoke(safeWrite);
-            }
-            else
-                myForm.MembersButton.Text = text;
-        }
-        public void RolesNumber(string text)
-        {
-            if (myForm.RolesButton.InvokeRequired)
-            {
-                Action safeWrite = delegate { RolesNumber(text); };
-                myForm.RolesButton.Invoke(safeWrite);
-            }
-            else myForm.RolesButton.Text = text;
-        }
-        public void TotalChannels(string text)
-        {
-            if (myForm.TotalChannelsButton.InvokeRequired)
-            {
-                Action safeWrite = delegate { TotalChannels(text); };
-                myForm.TotalChannelsButton.Invoke(safeWrite);
-            }
-            else
-                myForm.TotalChannelsButton.Text = text;
-        }
-        public void CreatedAt(string text)
-        {
-            if (myForm.CreatedAtButton.InvokeRequired)
-            {
-                Action safeWrite = delegate { CreatedAt(text); };
-                myForm.CreatedAtButton.Invoke(safeWrite);
-            }
-            else
-                myForm.CreatedAtButton.Text = text;
-
-        }
         public void MessageLog(string log)
         {
             if (myForm.MessagesRichBo.InvokeRequired)
